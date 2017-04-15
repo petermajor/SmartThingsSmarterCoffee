eaad622d93cae3f94920a69e0feb4c552a5af42b
Peter Major <peter@petermajor.co.uk>
2017-04-15 12:41:10 +0100

    use different ssdp server package

--- upnp.js
+++ upnp.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const Server = require('node-ssdp').Server;
+const ssdp = require('@achingbrain/ssdp');
 
 const RootUsn = 'upnp:rootdevice';
 const ServerUsn = 'urn:schemas-upnp-org:device:SmartThingsSmarterCoffee:1';
@@ -8,29 +8,32 @@ const ServerUsn = 'urn:schemas-upnp-org:device:SmartThingsSmarterCoffee:1';
 class Upnp
 {
     constructor() {
-        this.server = new Server();
+        this.bus = ssdp();
+        this.bus.on('error', console.error);
     }
 
     start() {
 
-        this.server.addUSN(RootUsn);
-        this.server.addUSN(ServerUsn);
-
-        this.server.on('advertise-alive', (heads) => {
-            console.log('advertise-alive', heads);
+        this.bus.advertise({
+            usn: ServerUsn,
+            // location: {
+            //     udp4: 'http://192.168.1.14:8080/ssdp/details.xml'
+            // },
+            details: {
+                URLBase: 'https://192.168.1.14:8001'
+            }
         });
 
-        this.server.on('advertise-bye', (heads)  => {
-            console.log('advertise-bye', heads);
-        });
-
-        this.server.start();
-
-        console.log(`Server is discoverable as ${ServerUsn}`);
-
-        process.on('exit', () => {
-            this.server.stop(); // advertise shutting down and stop listening 
+        // stop the server(s) from running - this will also send ssdp:byebye messages for all
+        // advertised services however they'll only have been sent once the callback is
+        // invoked so it won't work with process.on('exit') as you can only perform synchronous
+        // operations there
+        process.on('SIGINT',() => {
+            this.bus.stop(error => {
+                process.exit(error ? 1 : 0);
+            });
         });
     }
 }
+
 module.exports = new Upnp();
